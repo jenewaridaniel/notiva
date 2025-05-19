8d672fb857370ccd5e1d70a41729a52a4d672e15
Daniel <jenwaridaniel123@gmail.com>
2025-05-19 02:50:30 +0100

    saved

--- src/component/app/Upload.tsx
+++ src/component/app/Upload.tsx
@@ -1,6 +1,8 @@
 import uploadIcon from "../../assets/upload.png";
+import supabase from '../../config/supabase'
 
 const Upload = () => {
+  console.log(supabase)
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 p-4 md:p-8">
       {/* Header */}
